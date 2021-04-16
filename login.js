cc2eab43f2759af55486003400c19ea66ba151d7
Ancoder2009 <68029104+Ancoder2009@users.noreply.github.com>
2021-04-16 10:16:04 -0700

    Update login.js

--- login.js
+++ login.js
@@ -1,36 +1,38 @@
-const submit = document.getElementById("submit")
+body.onload = function() {
+  const submit = document.getElementById("submit")
 
-function login( data, username, password ) {
-  const XHR = new XMLHttpRequest(),
-        FD  = new FormData();
-  FD.append("username", str(username))
-  FD.append("password", str(password))
-  XHR.addEventListener( 'load', function( event ) {
-    return XHR.responseText
-  } );
-  XHR.addEventListener(' error', function( event ) {
-    return null
-  } );
-  // Set up our request
-  XHR.open( 'POST', 'https://api.scratchblox.tk/auth/login' );
-  XHR.send( FD );
-}
-
-
-const errorMessage = document.getElementById("errorMessage") 
-
-
-submit.addEventListener("click", function() {
-  var res = login()
-  if (res != null) {
-    if (res["status"] == 200) {
-      localStorage.setItem("_token", res["_token"])
-      window.location.assign("/")
-    } else {
-      errorMessage.innerHtml = "Username or Password is invalid."
-    }
-  } else {
-    errorMessage.innerHtml = "Something went wrong."
+  function login( data, username, password ) {
+    const XHR = new XMLHttpRequest(),
+          FD  = new FormData();
+    FD.append("username", str(username))
+    FD.append("password", str(password))
+    XHR.addEventListener( 'load', function( event ) {
+      return XHR.responseText
+    } );
+    XHR.addEventListener(' error', function( event ) {
+      return null
+    } );
+    // Set up our request
+    XHR.open( 'POST', 'https://api.scratchblox.tk/auth/login' );
+    XHR.send( FD );
   }
-  
-})
+
+
+  const errorMessage = document.getElementById("errorMessage") 
+
+
+  submit.addEventListener("click", function() {
+    var res = login()
+    if (res != null) {
+      if (res["status"] == 200) {
+        localStorage.setItem("_token", res["_token"])
+        window.location.assign("/")
+      } else {
+        errorMessage.innerHtml = "Username or Password is invalid."
+      }
+    } else {
+      errorMessage.innerHtml = "Something went wrong."
+    }
+
+  })
+}
