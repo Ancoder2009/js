7e730e9a020a0251d0474fcc5e931fe959f69393
Ancoder2009 <68029104+Ancoder2009@users.noreply.github.com>
2021-04-16 08:54:31 -0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,3 +1,40 @@
 /* Coppyright Ancoder 2021 */
 
-localStorage.get
+/* Apis */
+
+function getUserFromToken( data, _token ) {
+  const XHR = new XMLHttpRequest(),
+        FD  = new FormData();
+  // Push our data into our FormData object
+  FD.append("token", _token)
+  
+  XHR.addEventListener( 'load', function( event ) {
+    return XHR.responseText
+  } );
+  // Define what happens in case of error
+  XHR.addEventListener(' error', function( event ) {
+    return null
+  } );
+  // Set up our request
+  XHR.open( 'POST', 'https://api.scratchblox.tk/auth/token' );
+  // Send our FormData object; HTTP headers are set automatically
+  XHR.send( FD );
+};
+
+/* Check */
+
+var _token = localStorage.getItem("_token");
+var parentUrl = "https://blox.ancoder.repl.co";
+
+if(_token != null) {
+  var checkJson = getUserFromToken(_token);
+  if(checkJson != null) {
+    /* Insert DOM code here */
+  } else {
+    localStorage.removeItem("_token")
+    window.location.assign(window.location["href"])
+  };
+    
+} else {
+  window.location.assign(parentUrl + "/account/login/")
+};
