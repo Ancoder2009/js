0503d7b2975c3f7f9d519876875d32f828970baa
Ancoder2009 <68029104+Ancoder2009@users.noreply.github.com>
2021-04-16 17:42:25 -0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,54 +1,26 @@
 /* Coppyright Ancoder 2021 */
 
-/* Apis */
+var _token = localStorage.getItem("_token")
 
-function getUserFromToken( _token, returnFunc ) {
-  const XHR = new XMLHttpRequest(),
-        FD  = new FormData();
-  // Push our data into our FormData object
-  FD.append("token", _token)
-  
-  XHR.addEventListener( 'load', function( event ) {
-    returnFunc(XHR.responseText)
-  } );
-  // Define what happens in case of error
-  XHR.addEventListener(' error', function( event ) {
-    returnFunc(null)
-  } );
-  // Set up our request
-  XHR.open( 'POST', 'https://api.scratchblox.tk/auth/token', false );
-  // Send our FormData object; HTTP headers are set automatically
-  XHR.send( FD );
-};
+if (_token == null) {
+  /* Token good */
+  const formData = new FormData();
+  const photos = document.querySelector('input[type="file"][multiple]');
 
-/* Check */
+  formData.append('_token', _token);
 
-var _token = localStorage.getItem("_token");
-alert(_token)
-var parentUrl = "https://blox.ancoder.repl.co";
-
-if(_token != null) {
-  alert("null")
-  var checkJson
-  getUserFromToken(_token, function(result) {
-  checkJson = result;
+  fetch('https://api.scratchblox.tk/auth/token', {
+    method: 'POST',
+    body: formData,
+  })
+  .then(response => response.json())
+  .then(result => {
+    console.log('Success:', result);
+  })
+  .catch(error => {
+    console.error('Error:', error);
   });
-  alert(checkJson)
-  if(checkJson != null) {
-    alert("cj not null")
-    if(checkJson["status"] == 200) {
-      alert("200")
-      usernameLabels = document.getElementsByClassName("username")
-      var i
-      for(i = 0; i < usernameLabels.length; i++) {
-        usernameLabels[i].innerHTML =  usernameLabels[i].innerHTML + " " + checkJson["User"]
-      }
-    } else {
-      alert("remove")
-      localStorage.removeItem("_token")
-      window.location.assign(window.location["href"])
-    }
-  } else
-    localStorage.removeItem("_token")
-    window.location.assign(parentUrl + "/account/login")
-  }
+} else {
+  /* Token null */
+  window.location.assign("https://blox.ancoder.repl.co/account/login")
+}
